5c0fa50e1d1f0a120f3250c4f2fd80145b44a6f3
LRippeteau <112013655+LRippeteau@users.noreply.github.com>
2024-02-29 13:30:27 -0600

    Added Mia's styling to the LobbyOptions to mirror LandingOptions

--- frontend/src/components/LobbyOptions.tsx
+++ frontend/src/components/LobbyOptions.tsx
@@ -1,17 +1,44 @@
 import React from "react";
+import { CSSProperties } from "react";
 
 interface AuthProps {
   authState: string;
   setAuthState: React.Dispatch<React.SetStateAction<string>>;
 }
 
+const buttonStyle = {
+  backgroundColor: "#E66443", // Orange background
+  color: "white", // White Text
+  padding: "10px 20px", // Padding around text
+  margin: "5px", // Margin between buttons
+  borderRadius: "15px", // Rounded Corners
+  cursor: "pointer", // Pointer cursor on hover
+  border: "none", // No border
+  fontSize: "16px", // Text Size
+  width: "200px", // Fixed width for all buttons
+  fontFamily: "'Helvetica', sans-serif", // Font that we need to change
+  boxShadow: "inset 0 10px 15px -10px rgba(0, 0, 0, 0.4)", // Internal drop shadow
+};
+
+const buttonContainerStyle: CSSProperties = {
+  display: "flex",
+  alignItems: "center", // Center buttons horizontally
+  justifyContent: "center", // Center buttons vertically
+  flexDirection: "column", // Stack buttons vertically
+};
+
 const LobbyOptions: React.FC<AuthProps> = ({ authState, setAuthState }) => {
   return (
     <div>
       {authState === "default" && (
-        <div>
-          <button onClick={() => setAuthState("login")}>Join a Game</button>
-          <button onClick={() => setAuthState("signup")}>Create a Game</button>
+        <div style={buttonContainerStyle}>
+          <button onClick={() => setAuthState("join")} style={buttonStyle}>
+            {" "}
+            Join a Game
+          </button>
+          <button onClick={() => setAuthState("create")} style={buttonStyle}>
+            Create a Game
+          </button>
         </div>
       )}
       {authState === "join" && <p>Join a Game component here</p>}
