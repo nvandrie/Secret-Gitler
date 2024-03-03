76417ced5b4716773c22b82823454d7fb8640142
LRippeteau <112013655+LRippeteau@users.noreply.github.com>
2024-03-03 15:30:21 -0600

    Fixed dependencies on backend and frontend and gitignored (removed) node modules from git versioning (thank you Blaine)

--- backend/server.ts
+++ backend/server.ts
@@ -1,17 +1,16 @@
 // backend/server.ts
-import express, { Request, Response } from 'express';
-import cors from 'cors';
+import express, { Request, Response } from "express";
+import cors from "cors";
 
 const app = express();
 const port = 5001;
 
 app.use(cors());
 
-app.get('/api', (req: Request, res: Response) => {
-  res.send('Connected');
+app.get("/api", (req: Request, res: Response) => {
+  res.send("Connected");
 });
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
