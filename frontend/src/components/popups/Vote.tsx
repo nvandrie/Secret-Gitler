2b9752a039b20a36e45ebb6a2f0c72a6fb5124c4
miasiner <msiner2@huskers.unl.edu>
2024-03-28 11:46:10 -0500

    Styled Voting Popup

--- frontend/src/components/popups/Vote.tsx
+++ frontend/src/components/popups/Vote.tsx
@@ -1,33 +1,58 @@
-import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { RootState } from "../../store"
-import { toggleVotingActivity } from '../../slices/voteSlice'
-import '../../styling/popup.css';
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { RootState } from "../../store";
+import { toggleVotingActivity } from "../../slices/voteSlice";
+import "../../styling/popup.css";
+import ja from "/ja.jpg";
+import nein from "/nein.jpg";
 
 const Vote: React.FC = () => {
   const dispatch = useDispatch();
-  const votingActive = useSelector((state: RootState) => state.vote.votingActive);
+  const votingActive = useSelector(
+    (state: RootState) => state.vote.votingActive
+  );
 
   const handleToggleVoting = () => {
     dispatch(toggleVotingActivity());
   };
 
   return (
-    <div className="voteContainer">  
+    <div className="voteContainer">
       {votingActive && (
-        <div className={`vote${votingActive ? 'visibile-vote' : ''}`}>
-          <div className='voteBox'>
-            <div className="ya">
-              <button className="ya_button" onClick={handleToggleVoting}>ya</button>
-            </div>
-            <div className="nein">
-            <button className="nein_button" onClick={handleToggleVoting}>nein</button>
-            </div>
-            </div>
-          </div>)}
-    </div>  
+        <div className={`vote${votingActive ? "visibile-vote" : ""}`}>
+          <div className="voteContainer">
+            {votingActive && (
+              <div className={`vote${votingActive ? "visible-vote" : ""}`}>
+                <div className="voteBox">
+                  <p className="buttonsLabel">
+                    Do you wish for these indivials to have congressional power?
+                  </p>
+                  <div className="buttonsContainer">
+                    {" "}
+                    {/* Add this wrapper for buttons */}
+                    <div className="ya">
+                      <button
+                        className="ya_button"
+                        onClick={handleToggleVoting}
+                        style={{ backgroundImage: `url(${ja})` }}
+                      ></button>
+                    </div>
+                    <div className="nein">
+                      <button
+                        className="nein_button"
+                        onClick={handleToggleVoting}
+                        style={{ backgroundImage: `url(${nein})` }}
+                      ></button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
+      )}
+    </div>
   );
 };
 
 export default Vote;
-
