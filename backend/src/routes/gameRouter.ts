6f4856e72a676c1606b753fbc8625e17e4e57853
LRippeteau <112013655+LRippeteau@users.noreply.github.com>
2024-04-09 13:30:01 -0500

    update3

--- backend/src/routes/gameRouter.ts
+++ backend/src/routes/gameRouter.ts
@@ -1,18 +1,35 @@
 import express from "express";
-import { drawCards, newDeck, removeCard, getCard, startSelect } from "../controllers/deckController";
-import {addFascist, addLiberal, setPresident, setChancellor, createGame, initializePlayers} from "../controllers/gameplayController"
+import {
+  drawCards,
+  newDeck,
+  removeCard,
+  getCard,
+  startSelect,
+} from "../controllers/deckController";
+import {
+  addFascist,
+  addLiberal,
+  setPresident,
+  setChancellor,
+  createGame,
+  initializePlayers,
+} from "../controllers/gameplayController";
 
 const router = express.Router();
+
+// tracker routes
+router.post("api/new-tracker", newTracker);
+router.post("api/check-play-card", checkPlayCard);
 
 // deck routes
 router.post("/api/new-deck", newDeck);
 router.post("/api/draw-cards", drawCards);
 router.post("/api/remove-card", removeCard);
 router.post("/api/get-cards", getCard);
-router.post("/api/start-select", startSelect)
+router.post("/api/start-select", startSelect);
 
 //presidency and chancellor update routes
-router.post(`/api/initalize-players`, initializePlayers)
+router.post(`/api/initalize-players`, initializePlayers);
 router.post("/api/set-president", setPresident);
 router.post("/api/set-chancellor", setChancellor);
 
@@ -20,7 +37,4 @@ router.post("/api/set-chancellor", setChancellor);
 router.post("/api/create-game", createGame);
 router.post("/api/add-fascist", addFascist);
 router.post("/api/add-liberal", addLiberal);
-export default router
-
-
-
+export default router;
