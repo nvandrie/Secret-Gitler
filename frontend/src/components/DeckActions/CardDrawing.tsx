ca73e1bcba95b6a6a315fff261c606aab49ca09b
LRippeteau <112013655+LRippeteau@users.noreply.github.com>
2024-04-30 15:25:15 -0500

    Card clickability shown

--- frontend/src/components/DeckActions/CardDrawing.tsx
+++ frontend/src/components/DeckActions/CardDrawing.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import fascist_policy_card from "/policy_cards/fascist_policy.png";
 import liberal_policy_card from "/policy_cards/liberal_policy.png";
-import default_card from "/policy_cards/policy_card_back.png"
+import default_card from "/policy_cards/policy_card_back.png";
 import { useSelector, useDispatch } from "react-redux";
 import { RootState } from "../../store";
 import { setDiscardedCards } from "../../slices/deckSlice";
@@ -29,24 +29,24 @@ const CardDrawing: React.FC<CardDrawingProps> = ({ setSelectedCards }) => {
 
   const convertDataToCards = (data: string[]): Card[] => {
     return data.map((type) => {
-        let path;
-        switch (type) {
-            case "fascist":
-                path = fascist_policy_card;
-                break;
-            case "liberal":
-                path = liberal_policy_card;
-                break;
-            default:
-                path = default_card; 
-                break;
-        }
-        return {
-            type: type as "fascist" | "liberal" | "default",
-            path: path
-        };
+      let path;
+      switch (type) {
+        case "fascist":
+          path = fascist_policy_card;
+          break;
+        case "liberal":
+          path = liberal_policy_card;
+          break;
+        default:
+          path = default_card;
+          break;
+      }
+      return {
+        type: type as "fascist" | "liberal" | "default",
+        path: path,
+      };
     });
-};
+  };
 
   useEffect(() => {
     setIsCardsVisible(true);
@@ -54,45 +54,49 @@ const CardDrawing: React.FC<CardDrawingProps> = ({ setSelectedCards }) => {
   }, [currentCards]);
 
   useEffect(() => {
-    const socket = new WebSocket('ws://localhost:3000');
-  
+    const socket = new WebSocket("ws://localhost:3000");
+
     socket.onmessage = async (event) => {
       const message = JSON.parse(event.data);
-      if (message.type === 'select_cards') {
-        if (basicUserInfo?.name){
-          console.log(basicUserInfo?.name)
-          const identity = await searchRoleByName(basicUserInfo?.name) 
-          console.log(identity)
-          if(identity !== "chancellor"){
-            setSelectedCards(convertDataToCards(["default", "default"]))
+      if (message.type === "select_cards") {
+        if (basicUserInfo?.name) {
+          console.log(basicUserInfo?.name);
+          const identity = await searchRoleByName(basicUserInfo?.name);
+          console.log(identity);
+          if (identity !== "chancellor") {
+            setSelectedCards(convertDataToCards(["default", "default"]));
           } else {
-            setSelectedCards(message.cards)
-
+            setSelectedCards(message.cards);
           }
-          setIsCardsVisible(false)
+          setIsCardsVisible(false);
           const response = await axiosInstance.post("/api/get-cards");
-          dispatch(setDiscardedCards(response.data.discardCards.length - 2)) 
-      }}
+          dispatch(setDiscardedCards(response.data.discardCards.length - 2));
+        }
+      }
     };
-  
+
     return () => {
       socket.close();
     };
   }, []);
 
   const handleCardClick = async (index: number) => {
-    if (basicUserInfo?.name){
-      const identity = await searchRoleByName(basicUserInfo?.name) 
-      if(identity === "president"){
+    if (basicUserInfo?.name) {
+      const identity = await searchRoleByName(basicUserInfo?.name);
+      if (identity === "president") {
         if (isCardsVisible && selectedCards.length < 2) {
           const clickedCard = convertDataToCards([currentCards[index]])[0];
           setSelectedCardsState([...selectedCards, clickedCard]);
           if (selectedCards.length === 1) {
             setSelectedCards([...selectedCards, clickedCard]);
             setIsCardsVisible(false);
-            await axiosInstance.post("/api/start-select", {selectedCards: JSON.stringify([...selectedCards, clickedCard])})
+            await axiosInstance.post("/api/start-select", {
+              selectedCards: JSON.stringify([...selectedCards, clickedCard]),
+            });
           }
-    }}}
+        }
+      }
+    }
   };
 
   return (
@@ -101,7 +105,11 @@ const CardDrawing: React.FC<CardDrawingProps> = ({ setSelectedCards }) => {
         {convertDataToCards(currentCards).map((card, index) => (
           <div key={index} onClick={() => handleCardClick(index)}>
             {isCardsVisible && (
-              <img className="card" src={card.path} alt={card.type} />
+              <img
+                className={`card ${card.type}`}
+                src={card.path}
+                alt={card.type}
+              />
             )}
           </div>
         ))}
